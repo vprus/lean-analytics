de9e87ef83310ee9cb1b315a6cc3c2d7204c4db9
Vladimir Prus <vladimir@codesourcery.com>
2014-12-14 16:47:54 +0300

    Cleanup API.

--- demo/expenses/index.js
+++ demo/expenses/index.js
@@ -1,14 +1,30 @@
 
 var Model = LeanAnalytics.Model.extend({
 
-    makeMainMetrics_: function() {
-        var r = LeanAnalytics.Model.prototype.makeMainMetrics_.call(this);
+    makeRanges: function() {
+
+        // Since we use static sample data, use static date ranges.
+        return [
+            {name: "All time", range: [new Date("2013-03-29"), new Date("2014-09-26")]},
+            {name: "1 year", range: [new Date("2013-09-26"), new Date("2014-09-26")]}
+        ]
+    },
+
+    makeBaseMetrics: function() {
+        var r = LeanAnalytics.Model.prototype.makeBaseMetrics.call(this);
         r[0].name = "Amount";
         r[1].name = "Transaction Count";
         return r;
     },
 
-    makeAdditionalGroups_: function() {
+    makeDerivedMetrics: function() {
+        return [
+            this.makeGaussianDerivedMetric(),
+            this.makeLinearRegressionDerivedMetric()
+        ];
+    },
+
+    makeCategoryGroups: function() {
 
         var perCategory = this.crf.dimension(function(d) {                
             return d.category || d.name;
@@ -16,7 +32,7 @@ var Model = LeanAnalytics.Model.extend({
 
         return [
             {groupName: "Category", dimension: perCategory, group: perCategory.group()},
-            this.makePerDayOfWeekGroup_()
+            this.makePerDayOfWeekGroup()
         ];
     },
 
